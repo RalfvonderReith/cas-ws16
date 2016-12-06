186f3c623cf555aa6a05116c0ca4bbb5bfebcff6
Ralf von der Reith <ralf-von-der-reith@gmx.de>
2016-12-06 21:54:56 +0100

    plan fuer auftrag ausgearbeitet, boerse hinzugefuegt

--- src/haw/cas/praktikum/objects/Boerse.java
+++ src/haw/cas/praktikum/objects/Boerse.java
@@ -12,19 +12,21 @@ public class Boerse extends MObjekt implements Services {
 
     //TODO Geldtyp?!
 	//TODO Klasse Versteigerung, um das ganze etwas sauberer zu trennen // separation of concerns
-    private HashMap<Auftrag, Gebot> auftragsliste = new HashMap<Auftrag, Gebot>();
-
-    public Boerse(String name) {
+    private HashMap<Auftrag, Versteigerung> auftragsliste = new HashMap<Auftrag, Versteigerung>();
+    private Versteigerung.Typ typ;
+   
+    public Boerse(String name, Versteigerung.Typ typ) {
         super(name);
+        this.typ = typ;
     }
-
+    
     @Override
     public String getID() {
         return super.getUID();
     }
 
     public void addAuftrag(Auftrag auftrag) {
-        auftragsliste.put(auftrag, new Gebot(null, 0, auftrag, this));
+        auftragsliste.put(auftrag, typ.neueVersteigerung(this, auftrag));
     }
 
     public void removeAuftrag(Auftrag auftrag) {
@@ -32,22 +34,16 @@ public class Boerse extends MObjekt implements Services {
     }
 
     public int getGebot(Auftrag auftrag) {
-    	return auftragsliste.get(auftrag).getWert();
+    	return auftragsliste.get(auftrag).getHoechstGebot();
     }
     
     public synchronized boolean biete(Auftrag auftrag, int wert, LKW bieter) {
-    	Gebot altesGebot = auftragsliste.get(auftrag);
-    	if(altesGebot != null && wert > altesGebot.getWert()) {
-    		altesGebot.interrupt();
-    		auftragsliste.put(auftrag, new Gebot(bieter, wert, auftrag, this));
-    		return true;
-    	}
-    	return false;
+    	return auftragsliste.get(auftrag).biete(wert, bieter);
     }
     
     public synchronized void versteigert(Auftrag auftrag) {
     	removeAuftrag(auftrag);
-    	//TODO: packe auftrag irgendwo hin - z.b. in Umladebucht oder in Ladefläche von LKW
+    	//TODO: packe auftrag in Ladefläche von LKW
     }
     
     public List<Auftrag> bietetAn() {
--- src/haw/cas/praktikum/objects/Gebot.java
+++ src/haw/cas/praktikum/objects/Gebot.java
@@ -1,29 +1,14 @@
 package haw.cas.praktikum.objects;
 
 public class Gebot extends Thread {
-	private static final int timeout = 5000;
 	private final LKW bieter;
 	//TODO: Geldtyp
 	private final int wert;
-	private final Auftrag auftrag;
-	private final Boerse boerse; 
 	
-	public Gebot(LKW bieter, int wert, Auftrag auftrag, Boerse boerse) {
+	public Gebot(LKW bieter, int wert) {
 		this.bieter = bieter; 
 		this.wert = wert;
-		this.boerse = boerse;
-		this.auftrag = auftrag;
 		this.start();
-	}
-	
-	@Override
-	public void run() {
-		try {
-			Thread.sleep(5000);
-			boerse.versteigert(auftrag);
-		} catch(InterruptedException e) {
-			//überboten oder fehler
-		}
 	}
 	
 	public LKW getBieter() {
